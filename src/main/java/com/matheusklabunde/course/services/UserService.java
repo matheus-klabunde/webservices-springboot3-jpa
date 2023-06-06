7ed1a0f89e25ca83c110c95ac0a0814807a82edc
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-06 10:52:31 -0400

    Exception handler - update

--- src/main/java/com/matheusklabunde/course/services/UserService.java
+++ src/main/java/com/matheusklabunde/course/services/UserService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.matheusklabunde.course.entities.User;
 import com.matheusklabunde.course.repositories.UserRepository;
 import com.matheusklabunde.course.services.exceptions.DatabaseException;
 import com.matheusklabunde.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -47,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
